b175642292de3e55594efcc4808f7e9e80a6dc08
Juan Forero <forerojg@vcu.edu>
2023-02-16 04:19:49 +0000

    Completed String combination

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,13 +9,11 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("The Lab.combine method should combine two Strings together.");
         Lab cs = new Lab();
-
         System.out.println("Here's what Lab.combine returns when provided values \"abc\" and \"xyz\":");
         String p1val1 = "abc";
         String p1val2 = "xyz";
         String output1 = cs.combine(p1val1, p1val2);
         System.out.println(output1);
-
         System.out.println("Here's what Lab.combine returns when provided the value \"hello\" and \"world\":");
         String p2val1 = "hello";
         String p2val2 = "world";
--- src/test/java/CombineStringsTest.java
+++ src/test/java/CombineStringsTest.java
@@ -9,21 +9,26 @@ public class CombineStringsTest {
      */
     @Test
     public void combineTest1(){
+        Lab cs = new Lab();
         String input1 = "abc";
         String input2 = "xyz";
         String expected = "abcxyz";
-        String actual = cs.combine(input1, input2);
+        String actual = cs.combine(input1,input2);
         Assert.assertEquals(expected,actual);
+        /**return a + b;**/
     }
+    
     /**
      * The combination of strings 123 and 456 should result in 123456.
      */
     @Test
     public void combineTest2(){
-        String input1 = "123"; 
-        String input2 = "456";
+        String input1 = Integer.toString(123); 
+        String input2 =  Integer.toString(456);
         String expected = "123456";
-        String actual = cs.combine(input1, input2);
+        String actual = cs.combine( input1,input2);
         Assert.assertEquals(expected,actual);
+        /*** return a + b;**/
+        
     }
 }
